80f6eff69c36975fd8dfa3996380842eed311586
Martin Hellström <martin.hellstrom@dgc.se>
2017-08-23 16:00:47 +0200

    adds support for logging in json format

--- hooks/neutron_api_context.py
+++ hooks/neutron_api_context.py
@@ -448,3 +448,21 @@ class MidonetContext(context.OSContextGenerator):
                 if self.context_complete(ctxt):
                     return ctxt
         return {}
+
+class NeutronApiLoggingContext(context.OSContextGenerator):
+
+    def __call__(self):
+        ctxt = {}
+        debug = config('debug')
+        if debug:
+            ctxt['root_level'] = 'DEBUG'
+        log_level = config('log-level')
+        log_level_accepted_params = ['WARNING', 'INFO', 'DEBUG', 'ERROR']
+        if log_level in log_level_accepted_params:
+            ctxt['log_level'] = config('log-level')
+        else:
+            log("log-level must be one of the following states "
+                "(WARNING, INFO, DEBUG, ERROR) keeping the current state.")
+            ctxt['log_level'] = None
+
+        return ctxt
